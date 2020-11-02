782b888f846fc10f605cd605d66297c0794e42dc
ayushcsi <61983787+ayushcsi@users.noreply.github.com>
2020-11-02 15:53:45 +0530

    shelves commit

---
+++ src/main/java/com/library/shelves/ShelvesApplication.java
@@ -0,0 +1,13 @@
+package com.library.shelves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/shelves/ShelvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.shelves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
